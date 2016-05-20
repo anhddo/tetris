02424d1cf44eabafdab84a18c93ac2ab6e52a96c
oong <anhddo93@gmail.com>
2016-05-21 02:03:55 +0700

    Add simple test

---
+++ Assets/Editor/LogicTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+
+public class LogicTest
+{
+
+
+    const int L = 0;
+    const int SQUARE = 1;
+    [Test]
+    public void FirstTest()
+    {
+        int[,] currentState = { {0,0,0},
+                                {0,0,0},
+                                {0,0,0},
+                                {0,0,0}};
+        int[,] expected = { {1,0,0},
+                               {1,0,0},
+                               {1,0,0},
+                               {1,0,0}};
+        int inputType = L;
+        var result = calc(currentState, inputType,new Vector2(0,0));
+        Assert.AreEqual(expected, result);
+    }
+    [Test]
+    public void SecondTest()
+    {
+        int[,] currentState = { {0,0,0},
+                                {0,0,0},
+                                {0,0,0},
+                                {0,0,0}};
+        int[,] expected = { {0,0,0},
+                                {0,0,0},
+                                {1,1,0},
+                                {1,1,0}};
+        int inputType = L;
+        var result = calc(currentState, inputType, new Vector2(0,0));
+        Assert.AreEqual(expected, result);
+    }
+
+    private int[,] calc(int[,] currentState, int inputType, Vector2 position)
+    {
+        int[,] result = { {1,0,0},
+                               {1,0,0},
+                               {1,0,0},
+                               {1,0,0}};
+        return result;
+    }
+}
